dfe23dacd56b4a14b71f2acd503ef400fc4803f4
Piotr Sokołowski <piotr@sokolowscy.net>
2016-11-04 12:33:57 +0100

    UPD: Remove ancient tests

--- test/helper.rb
+++
@@ -1,8 +0,0 @@
-$:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'test/unit'
-require 'point'
-
-Point.username = "apitest@pointhq.com"
-Point.apitoken = `git config point.apitestkey`.chomp
-
-puts "Working on '#{Point.site}' as '#{Point.username}'"
