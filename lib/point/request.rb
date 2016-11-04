55eb4a7d78434118be2640b7cd8489b526fbb638
Piotr Sokołowski <piotr@sokolowscy.net>
2016-11-04 12:42:52 +0100

    REF: Whitespaces

--- lib/point/request.rb
+++ lib/point/request.rb
@@ -1,22 +1,22 @@
 module Point
   class Request
-    
+
     attr_reader :path, :method
     attr_accessor :data
-    
+
     def initialize(path, method = :get)
       @path = path
       @method = method
     end
-    
+
     def success?
       @success || false
     end
-    
+
     def output
       @output || nil
     end
-    
+
     ## Make a request to the Point API using net/http. Data passed can be a hash or a string
     ## Hashes will be converted to JSON before being sent to the remote service.
     def make
@@ -25,7 +25,7 @@ module Point
       http_request.basic_auth(Point.username, Point.apitoken)
       http_request.add_field("Accept", "application/json")
       http_request.add_field("Content-type", "application/json")
-      
+
       http = Net::HTTP.new(uri.host, uri.port)
       data = self.data.to_json if self.data.is_a?(Hash) && self.data.respond_to?(:to_json)
       http_result = http.request(http_request, data)
@@ -46,10 +46,10 @@ module Point
       end
       self
     end
-    
+
     private
-    
-    def http_class  
+
+    def http_class
       case @method
       when :post    then Net::HTTP::Post
       when :put     then Net::HTTP::Put
@@ -58,6 +58,6 @@ module Point
         Net::HTTP::Get
       end
     end
-    
+
   end
 end
